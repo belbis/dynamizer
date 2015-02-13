95f57bc4c773d3d88fdd33ac16fcb223d47fd5ad
mike <mike@martini-corp.com>
2015-02-12 16:43:15 -0800

    updated errors

--- src/errors.js
+++ src/errors.js
@@ -1,24 +1,35 @@
 
 
-// standard import
+// standard imports
 var util = require("util");
 
 /**
  * InvalidParametersError
  *
- * thrown when an invalid Object is passed
+ * thrown when invalid parameters are passed to a function
+ * @param msg {string} the message to be
  * @constructor
  */
-function InvalidParametersError() {
-  this.message = "Invalid Parameters.";
+function InvalidParametersError(msg) {
+  this.name = "InvalidParametersError";
+  this.message = msg || "Invalid Parameters.";
 }
 util.inherits(InvalidParametersError, Error);
 
-function NotImplementedError() {
-  this.message = "Not Implemented.";
+/**
+ * NotImplementedError
+ *
+ * thrown when function called is not implemented
+ * @param msg {string} the message to be displayed
+ * @constructor
+ */
+function NotImplementedError(msg) {
+  this.name = "NotImplementedError";
+  this.message = msg || "Not Implemented.";
 }
 util.inherits(NotImplementedError, Error);
 
+// export module
 module.exports = {
   InvalidParametersError: InvalidParametersError,
   NotImplementedError: NotImplementedError
--- test/errorTest.js
+++ test/errorTest.js
@@ -7,16 +7,32 @@ var errors = require(__dirname + "/../src/errors"),
   InvalidParametersError = errors.InvalidParametersError;
 
 var invalidParametersErrorTest = function(test) {
+  test.expect(5);
+
   test.throws(function() {
     throw new InvalidParametersError()
   }, InvalidParametersError);
+  var display = "display message";
+  test.equal((new InvalidParametersError(display)).message, display);
+  test.equal((new InvalidParametersError()).message, "Invalid Parameters.");
+  test.ok((new InvalidParametersError()).name, "InvalidParametersError");
+  test.ok((new InvalidParametersError()) instanceof Error);
+
   test.done();
 };
 
 var notImplementedErrorTest = function(test) {
+  test.expect(5);
+
   test.throws(function() {
     throw new NotImplementedError
   }, NotImplementedError);
+  var display = "display message";
+  test.equal((new NotImplementedError(display)).message, display);
+  test.equal((new NotImplementedError()).message, "Not Implemented.");
+  test.ok((new NotImplementedError()).name, "NotImplementedError");
+  test.ok((new NotImplementedError()) instanceof Error);
+
   test.done();
 };
 
